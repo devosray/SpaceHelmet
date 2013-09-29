33f5e82be338aad5e51d3148d7c5d0096fa89506
Raymond de Vos <devosray@gmail.com>
2013-09-29 17:56:32 +0200

    Initial Commit

---
+++ SpaceHelmet/src/com/github/devosray/SpaceHelmet/SHPlugin.java
@@ -0,0 +1,22 @@
+
+package com.github.devosray.SpaceHelmet;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Raymond
+ */
+public class SHPlugin extends JavaPlugin{
+    
+    @Override
+    public void onEnable(){
+        //TODO onEnable code
+    }
+    
+    @Override
+    public void onDisable(){
+        //TODO onDisable code
+    }
+    
+}
